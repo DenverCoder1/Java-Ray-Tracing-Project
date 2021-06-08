9ee38405ac4872011b1b5d87be4db1048837f6ad
Jonah Lawrence <jonah@freshidea.com>
2021-06-09 01:11:54 +0300

    Added custom scene

--- src/unittests/lights/ReflectionRefractionTests.java
+++ src/unittests/lights/ReflectionRefractionTests.java
@@ -6,6 +6,7 @@ package unittests.lights;
 import org.junit.Test;
 
 import elements.*;
+import geometries.Plane;
 import geometries.Sphere;
 import geometries.Triangle;
 import primitives.*;
@@ -119,4 +120,103 @@ public class ReflectionRefractionTests {
 		render.renderImage();
 		render.writeToImage();
 	}
+
+	/**
+	 * Produce a picture of a bird-like figure
+	 */
+	@Test
+	public void customScene() {
+		Camera camera = new Camera(new Point3D(0, 0, 1000), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
+				.setViewPlaneSize(150, 150).setDistance(1000);
+
+		scene.geometries.add( //
+				new Sphere(new Point3D(0, 0, -50), 50) //
+						.setEmission(new Color(java.awt.Color.GREEN)) //
+						.setMaterial(new Material().setKD(0.4).setKS(0.3).setShininess(100).setKT(0.3)),
+				new Sphere(new Point3D(-25, 5, -20), 25) //
+						.setEmission(new Color(java.awt.Color.WHITE)) //
+						.setMaterial(new Material().setKD(0.5).setKS(0.5).setShininess(100).setKT(0.9)),
+				new Sphere(new Point3D(-30, 5, 1), 5) //
+						.setEmission(new Color(java.awt.Color.BLACK)) //
+						.setMaterial(new Material().setKD(0.5).setKS(0.5).setShininess(100)),
+				new Sphere(new Point3D(25, 5, -20), 25) //
+						.setEmission(new Color(java.awt.Color.WHITE)) //
+						.setMaterial(new Material().setKD(0.5).setKS(0.5).setShininess(100).setKT(0.5)),
+				new Sphere(new Point3D(30, 5, 1), 5) //
+						.setEmission(new Color(java.awt.Color.BLACK)) //
+						.setMaterial(new Material().setKD(0.5).setKS(0.5).setShininess(100)),
+				new Sphere(new Point3D(30, 5, 1), 5) //
+						.setEmission(new Color(java.awt.Color.BLACK)) //
+						.setMaterial(new Material().setKD(0.5).setKS(0.5).setShininess(100)),
+				new Triangle(new Point3D(-10, -18, -5), new Point3D(10, -18, -5), new Point3D(0, -38, 5)) //
+						.setEmission(new Color(java.awt.Color.ORANGE)) //
+						.setMaterial(new Material().setKR(0.3)));
+
+		scene.lights.add( //
+				new SpotLight(new Color(1000, 600, 0), new Point3D(-100, -100, 500), new Vector(-1, -1, -2)) //
+						.setKL(0.0004).setKQ(0.000006));
+
+		scene.setCamera(camera);
+
+		Render render = new Render() //
+				.setImageWriter(new ImageWriter("customScene", 500, 500)) //
+				.setRayTracer(new BasicRayTracer(scene));
+		render.renderImage();
+		render.writeToImage();
+	}
+
+	/**
+	 * Produce a picture of a bird like figure inside two triangular mirrors
+	 */
+	@Test
+	public void customScene2() {
+		Camera camera = new Camera(new Point3D(0, 0, 10000), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
+				.setViewPlaneSize(2500, 2500).setDistance(10000); //
+
+		scene.geometries.add( //
+				new Sphere(new Point3D(-500.0 + 0, -500.0 + 0, -500.0 - 50), 50) //
+						.setEmission(new Color(java.awt.Color.GREEN)) //
+						.setMaterial(new Material().setKD(0.4).setKS(0.3).setShininess(100).setKT(0.3)),
+				new Sphere(new Point3D(-500.0 - 25, -500.0 + 5, -500.0 - 20), 25) //
+						.setEmission(new Color(java.awt.Color.WHITE)) //
+						.setMaterial(new Material().setKD(0.5).setKS(0.5).setShininess(100).setKT(0.9)),
+				new Sphere(new Point3D(-1030, -500.0 + 5, -500.0 + 1), 5) //
+						.setEmission(new Color(java.awt.Color.BLACK)) //
+						.setMaterial(new Material().setKD(0.5).setKS(0.5).setShininess(100)),
+				new Sphere(new Point3D(-500.0 + 25, -500.0 + 5, -500.0 - 20), 25) //
+						.setEmission(new Color(java.awt.Color.WHITE)) //
+						.setMaterial(new Material().setKD(0.5).setKS(0.5).setShininess(100).setKT(0.5)),
+				new Sphere(new Point3D(-500.0 + 30, -500.0 + 5, -500.0 + 1), 5) //
+						.setEmission(new Color(java.awt.Color.BLACK)) //
+						.setMaterial(new Material().setKD(0.5).setKS(0.5).setShininess(100)),
+				new Sphere(new Point3D(-500.0 + 30, -500.0 + 5, -500.0 + 1), 5) //
+						.setEmission(new Color(java.awt.Color.BLACK)) //
+						.setMaterial(new Material().setKD(0.5).setKS(0.5).setShininess(100)),
+				new Triangle(new Point3D(-500.0 - 10, -500.0 - 18, -500.0 - 5),
+						new Point3D(-500.0 + 10, -500.0 - 18, -500.0 - 5), new Point3D(-500.0 + 0, -500.0 - 38, -500.0 + 5)) //
+								.setEmission(new Color(java.awt.Color.ORANGE)) //
+								.setMaterial(new Material().setKR(0.3)),
+				new Triangle(new Point3D(1500, -1500, -1500), new Point3D(-1500, 1500, -1500), new Point3D(670, 670, 3000)) //
+						.setEmission(new Color(20, 20, 20)) //
+						.setMaterial(new Material().setKR(1)),
+				new Triangle(new Point3D(1500, -1500, -1500), new Point3D(-1500, 1500, -1500), new Point3D(-1500, -1500, -2000)) //
+						.setEmission(new Color(20, 20, 20)) //
+						.setMaterial(new Material().setKR(0.5)));
+
+		scene.lights.add(new SpotLight(new Color(1020, 400, 400), new Point3D(-750, -750, -150), new Vector(-1, -1, -4)) //
+				.setKL(0.00001).setKQ(0.000005));
+
+		scene.lights.add( //
+				new SpotLight(new Color(1000, 600, 0), new Point3D(-100, -100, 500), new Vector(-1, -1, -2)) //
+						.setKL(0.0004).setKQ(0.000006));
+
+		scene.setCamera(camera);
+
+		Render render = new Render() //
+				.setImageWriter(new ImageWriter("customScene2", 500, 500)) //
+				.setRayTracer(new BasicRayTracer(scene));
+		render.renderImage();
+		render.writeToImage();
+	}
+
 }
